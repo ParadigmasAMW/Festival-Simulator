ceaf3fcf59de9e8d5358e311360e2c6c349a8ca0
Wilker Mesquita <wilker-mc@hotmail.com>
2015-06-20 23:26:26 -0300

    Initializing Festival Simulator
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

---
+++ src/Companion.java
@@ -0,0 +1,41 @@
+import jade.core.Agent;
+
+
+public class Companion extends Agent implements Public{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void applause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void boo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void criticize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void praise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showBoobs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Public.java
@@ -0,0 +1,10 @@
+
+public interface Public {
+	
+	public void applause();
+	public void boo();
+	public void criticize();
+	public void praise();
+	public void showBoobs();
+
+}
