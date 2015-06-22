db615059d0978df1357d56bd2565ed66cc35e85e
msfernandes <matheus.souza.fernandes@gmail.com>
2015-06-22 11:26:25 -0300

    Update festival agent
    Signed-off-by: msfernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Álvaro Fernando <alvarofernandoms@gmail.com>
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

--- src/agents/FestivalAgent.java
+++ src/agents/FestivalAgent.java
@@ -1,4 +1,7 @@
 package agents;
+import java.util.Random;
+import java.util.Vector;
+
 import examples.content.Receiver;
 import gui.FestivalGui;
 import jade.core.AID;
@@ -9,15 +12,22 @@ import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.JADEAgentManagement.CreateAgent;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.tools.testagent.ReceiveCyclicBehaviour;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.PlatformController;
 
 
 public class FestivalAgent extends Agent {
 
 	private static final long serialVersionUID = 1L;
 	private FestivalGui gui;
+	
+	protected Vector publicList = new Vector();
+	protected int publicCount = 0;
 	
 	protected void setup(){
 		gui = new FestivalGui(this);
@@ -85,7 +95,14 @@ public class FestivalAgent extends Agent {
 	}
 	
 	public void startFestival(){
-		addBehaviour(new StartFestival());		
+		PlatformController c = getContainerController();
+		try {
+			AgentController agent = c.createNewAgent("Banda 1", "agents.BandAgent", null);
+			agent.start();
+		} catch (Exception e) {}
+		
+		addBehaviour(new StartFestival());
+		invitePublic(10);
 	}
 	
 	public void changeBand(){
@@ -94,5 +111,26 @@ public class FestivalAgent extends Agent {
 	
 	public void finishFestival(){
 		addBehaviour(new FinishFestival());
+	}
+	
+	protected void invitePublic(int publicSize) {
+		PlatformController c = getContainerController();
+		try {
+			for (int i = 0; i < publicSize; i++) {
+				Random rand = new Random();
+				int number = rand.nextInt(11) + 1;
+				
+				AgentController agent;
+				if (number % 2 == 0){
+					String localName = "Enthusiast " + i;
+					agent = c.createNewAgent(localName, "agents.EnthusiastAgent", null);
+				} else {
+					String localName = "Companion " + i;
+					agent = c.createNewAgent(localName, "agents.CompanionAgent", null);
+				}
+				agent.start();
+				publicList.add(agent);
+			}
+		} catch (Exception e) {}
 	}
 }
